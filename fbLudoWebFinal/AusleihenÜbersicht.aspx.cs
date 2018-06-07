5ff5d6749473a4845d30d25a9f67cfa33c5eea3f
lgrue <luca.grueninger@gmx.ch>
2018-06-07 09:32:50 +0200

    .

--- fbLudoWebFinal/AusleihenÜbersicht.aspx.cs
+++ fbLudoWebFinal/AusleihenÜbersicht.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
@@ -238,10 +239,32 @@ namespace fbLudoWebFinal
             Response.Redirect(Request.RawUrl);
         }
 
-        private void LoadData(String userId)
+        private void LoadData(string userId)
         {
             _context = new fbLudoDBEntities();
-            var list = _context.Ausleihe.ToList();
+            var list = _context.Ausleihe.Where(x => x.PersonenID == userId).ToList();
+
+            var table = ConvertToDataTable(list);
+
+            tblAusleihenAktiv.DataBind(table);
+        }
+
+        public DataTable ConvertToDataTable<T>(IList<T> data)
+        {
+            PropertyDescriptorCollection properties =
+               TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+
         }
     }
 }
